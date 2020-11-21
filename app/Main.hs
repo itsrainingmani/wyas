a39ab927ba30345b0042cda97ea9155b238777b3
Manikandan Sundararajan <mani@hey.com>
2020-11-21 03:37:09 -0500

    Read input from stdin and print out to stdout

--- app/Main.hs
+++ app/Main.hs
@@ -5,6 +5,10 @@ import System.Environment
 -- IO () is an IO Action carrying along values of unit type ()
 main :: IO ()
 main = do
-  args <- getArgs
-  -- read 2 nums, add them and print the result
-  print (read (head args) + read (args !! 1))
+  putStrLn "Please enter your name:\n"
+  name <- getLine
+  putStrLn ("Hello, " ++ name)
+
+-- args <- getArgs
+-- read 2 nums, add them and print the result
+-- print (read (head args) + read (args !! 1))
