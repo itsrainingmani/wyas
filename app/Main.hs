d3ee2eb4cfa0e8fa7b4fd7ffad7a8c5250559bf1
Manikandan Sundararajan <mani@hey.com>
2020-11-21 03:21:17 -0500

    Add exercise answer 1 and 2

--- app/Main.hs
+++ app/Main.hs
@@ -2,7 +2,9 @@ module Main where
 
 import System.Environment
 
+-- IO () is an IO Action carrying along values of unit type ()
 main :: IO ()
 main = do
   args <- getArgs
-  putStrLn ("Hello, " ++ head args)
+  -- read 2 nums, add them and print the result
+  print (read (head args) + read (args !! 1))
