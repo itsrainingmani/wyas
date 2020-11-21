5f0885489b02257c5c3d1bc97b5aa5db946f0858
Manikandan Sundararajan <mani@hey.com>
2020-11-21 03:53:22 -0500

    Build basic parsers

--- app/Main.hs
+++ app/Main.hs
@@ -1,13 +1,29 @@
 module Main where
 
 import System.Environment
+import Text.ParserCombinators.Parsec hiding (spaces)
+
+-- Define a parser that recognizes one of the symbols allowed in Scheme identifiers
+symbol :: Parser Char
+symbol = oneOf "!#$%&|*+-/:<=>?@^_~"
+
+spaces :: Parser ()
+spaces = skipMany1 space
+
+readExpr :: String -> String
+readExpr input = case parse (spaces >> symbol) "lisp" input of
+  Left err -> "No match:" ++ show err
+  Right _val -> "Found value"
 
 -- IO () is an IO Action carrying along values of unit type ()
 main :: IO ()
 main = do
-  putStrLn "Please enter your name:\n"
-  name <- getLine
-  putStrLn ("Hello, " ++ name)
+  (expr : _) <- getArgs
+  putStrLn (readExpr expr)
+
+-- putStrLn "Please enter your name:\n"
+-- name <- getLine
+-- putStrLn ("Hello, " ++ name)
 
 -- args <- getArgs
 -- read 2 nums, add them and print the result
