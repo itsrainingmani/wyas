57589e4c8179678307475c6aa09aef05c1438bfe
Manikandan Sundararajan <mani@hey.com>
2020-11-22 05:27:05 -0500

    Define ADS for a Lisp value and a parser for it

--- app/Main.hs
+++ app/Main.hs
@@ -3,12 +3,39 @@ module Main where
 import System.Environment
 import Text.ParserCombinators.Parsec hiding (spaces)
 
--- Define a parser that recognizes one of the symbols allowed in Scheme identifiers
+-- Data Structure that can hold any Lisp value
+-- This is an Algebraic Data Type - Defines a set of possible values
+data LispVal
+  = Atom String
+  | List [LispVal]
+  | DottedList [LispVal] LispVal
+  | Number Integer
+  | String String
+  | Bool Bool
+
+-- Parser that recognizes one of the symbols allowed in Scheme identifiers
 symbol :: Parser Char
 symbol = oneOf "!#$%&|*+-/:<=>?@^_~"
 
 spaces :: Parser ()
 spaces = skipMany1 space
+
+parseString :: Parser LispVal
+parseString = do
+  char '"'
+  x <- many (noneOf "\"")
+  char '"'
+  return $ String x
+
+parseAtom :: Parser LispVal
+parseAtom = do
+  first <- letter <|> symbol
+  rest <- many (letter <|> digit <|> symbol)
+  let atom = first : rest
+  return $ case atom of
+    "#t" -> Bool True
+    "#f" -> Bool False
+    _ -> Atom atom
 
 readExpr :: String -> String
 readExpr input = case parse (spaces >> symbol) "lisp" input of
